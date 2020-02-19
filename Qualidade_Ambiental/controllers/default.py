31056698d47a91e683e97801b0e5aadd356cdd2c
charleslll <call.jmail@gmail.com>
2020-02-19 17:02:52 -0300

    Adicionando projeto Qualidade_Ambiental

---
+++ Qualidade_Ambiental/controllers/default.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------------
+# This is a sample controller
+# this file is released under public domain and you can use without limitations
+# -------------------------------------------------------------------------
+
+# ---- example index page ----
+
+
+def index():
+    response.flash = T("Bem Vindo!")
+    return dict(sistema=T('Sistema de Dados do Dpto. de Qualidade Ambiental!'))
+
+# ---- API (example) -----
+@auth.requires_login()
+def api_get_user_email():
+    if not request.env.request_method == 'GET': raise HTTP(403)
+    return response.json({'status':'success', 'email':auth.user.email})
+
+    
+
+# ---- Smart Grid (example) -----
+@auth.requires_membership('admin') # can only be accessed by members of admin groupd
+def grid():
+    response.view = 'generic.html' # use a generic view
+    tablename = request.args(0)
+    if not tablename in db.tables: raise HTTP(403)
+    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=False, editable=False)
+    return dict(grid=grid)
+
+# ---- Embedded wiki (example) ----
+def wiki():
+    auth.wikimenu() # add the wiki to the menu
+    return auth.wiki() 
+
+# ---- Action for login/register/etc (required for auth) -----
+def user():
+    """
+    exposes:
+    http://..../[app]/inicio/user/login
+    http://..../[app]/inicio/user/logout
+    http://..../[app]/inicio/user/register
+    http://..../[app]/inicio/user/profile
+    http://..../[app]/inicio/user/retrieve_password
+    http://..../[app]/inicio/user/change_password
+    http://..../[app]/inicio/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
+    """
+    return dict(form=auth())
+
+# ---- action to server uploaded static content (required) ---
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/inicio/download/[filename]
+    """
+    return response.download(request, db)
---
+++ Qualidade_Ambiental/modules/validador.py
@@ -0,0 +1,90 @@
+
+
+
+
+class IS_CHKBOX01:
+    def __init__(self, on, off, error_message='Erro!'):
+        self._on = on
+        self._off = off
+        self.e = error_message
+
+    def parsed(self, value):
+        return self._on if value == 'on' else self._off
+
+    def __call__(self, value):
+
+        if value in (None, 'on', 'off'):
+            return (self.parsed(value), None)
+
+        return (value, self.e)
+
+
+    
+class IS_CPF(object):
+    def __init__(self, format=True, error_message='Digite apenas os numeros!'):
+        self.format = format
+        self.error_message = error_message
+
+    def __call__(self, value):
+
+        def valida(value):
+
+            def calcdv(numb):
+                result = int()
+                seq = reversed(((range(9, id_type[1], -1) * 2)[:len(numb)]))
+                #return (value,'to fundo1')
+                for digit, base in zip(numb, seq):
+                    result += int(digit) * int(base)
+
+                dv = result % 11
+                #return (value,'to fundo1'+str(dv))
+                return (dv - 10) and dv or 0
+
+            id_type = ['CPF', -1]
+
+            numb, xdv = value[:-2], value[-2:]
+
+            dv1 = calcdv(numb)
+            #return (value,'entrei'+str(dv1))
+            dv2 = calcdv(numb + str(dv1))
+            return (('%d%d' % (dv1, dv2) == xdv and True or False), id_type[0])
+
+
+        try:
+            cpf = str(value)
+            #return(cpf,'aquiok'+str(len(cpf)==11))
+            if len(cpf) >= 11:
+
+                #return (value, 'cpf acima de 11')
+                c = []
+                for d in cpf:
+                    if d.isdigit():
+                        c.append(d)
+                cl = str(''.join(c))
+                #return (value, 'cpf incorreto'+str(cl))
+                if len(cl) == 11:
+                    if valida(cl)[0] == True:
+                        return (cl, None)
+                    else:
+                        return (cl, 'cpf invalido')
+                elif len(cl) < 11:
+                    return (cl, 'cpf incompleto')
+                else:
+                    return (cl, 'cpf tem mais de 11 digitos')
+            else:
+                return (value, 'cpf deve estar no formato 000.000.000-00')
+                #return(cpf,'aquiok'+str(len(cpf)==11))
+
+
+        except:
+            return (value, 'algum erro' + str(value))
+
+#def formatter(self, value):
+        #if value ==11:
+#        formatado = value[0:3] + '.' + value[3:6] + '.' + value[6:9] + '-' + value[9:11]
+        #else:
+        #    formatado=value
+        #formatado = value
+#	return formatado '''
+
+
